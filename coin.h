fe23960e907c56c295e796f18efbbc6104ae0eb4
Matthieu 'JP' DERASSE <github@derasse.fr>
2020-07-20 23:13:03 +0200

    Implement Vericoin

--- coin.h
+++ coin.h
@@ -7,28 +7,28 @@
 
 #include <string>
 
-static const int FIRST_PROTOCOL_VERSION = 80001;
-static const int PROTOCOL_VERSION = 80002;
-static const int REQUIRE_PROTOCOL_VERSION = 80001;
+static const int FIRST_PROTOCOL_VERSION = 70012;
+static const int PROTOCOL_VERSION = 70012;
+static const int REQUIRE_PROTOCOL_VERSION = 70012;
 
-static const int MAINNET_MIN_HEIGHT = 180000;
-static const int TESTNET_MIN_HEIGHT = 50000;
+static const int MAINNET_MIN_HEIGHT = 1751415;
+static const int TESTNET_MIN_HEIGHT = 500000;
 
-static const std::string seeder_version = "/verium-seeder:1.11/";
+static const std::string seeder_version = "/vericoin-seeder:1.8.0/";
 
 static const std::string mainnet_seeds[] = {"206.189.59.89",
                                             "206.189.209.81",
                                             "206.189.145.110",
-                                            "emea.vrm.vericonomy.com",
-                                            "amer.vrm.vericonomy.com",
-                                            "apac.vrm.vericonomy.com",
-                                            "seed.vrm.mining-pool.ovh",
+                                            "emea.vrc.vericonomy.com",
+                                            "amer.vrc.vericonomy.com",
+                                            "apac.vrc.vericonomy.com",
+                                            "seed.vrc.mining-pool.ovh",
                                             ""};
 
 static const std::string testnet_seeds[] = {"", ""};
 
-static const int mainnet_port = 32988;
-static const int testnet_port = 36988;
+static const int mainnet_port = 58684;
+static const int testnet_port = 48684;
 
 static unsigned char pchMessageStart[4] = { 0x70, 0x35, 0x22, 0x05 };
 static unsigned char pchMessageStart_testnet[4] = { 0x70, 0x35, 0x22, 0x05 };
